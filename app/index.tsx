b4ba1b9ec2dc47c74e41f4ecb5556f7ee38b68cb
ASUS <manuthkausilu20031018@gmail.com>
2025-08-28 10:52:26 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import "./../global.css"
+
+
+const Index = () => {
+  return (
+    <View>
+      <Text className="text-red-600 font-bold">Index</Text>
+    </View>
+  )
+}
+
+export default Index
---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJlGf_Y2JYZ5CHrv5f-mldZYafnKPfPKg",
+  authDomain: "revenueapp-c9544.firebaseapp.com",
+  projectId: "revenueapp-c9544",
+  storageBucket: "revenueapp-c9544.firebasestorage.app",
+  messagingSenderId: "997039066236",
+  appId: "1:997039066236:web:83c9c3aa8380d9e5e5cdf9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
